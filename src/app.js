1e8009466baab7a13a348d4c24d45418bd3f99b9
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-08-08 23:30:29 -0400

    Fixed character in app.js that was causing failure

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config’)
+const { NODE_ENV } = require('./config')
 
 const app = express()
 
